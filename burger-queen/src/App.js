040ab4e36d2d2568709e8ea3c1872b6f2f923511
Jack Mattar <jackeline_mattar@hotmail.com>
2019-12-11 19:02:17 -0300

    adicionando arquivos firebase

--- burger-queen/src/App.js
+++ burger-queen/src/App.js
@@ -1,32 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {db} from './firebase.js'
 
 
+
+
 function App() {
-   db.collection('Menus')
-  .doc('teste')
-  .get()
-  .then((doc)=>{
-    console.log(doc.data().preço)
+   db.collection('Café da Manhã')
+   .get()
+   .then((querySnapshot) => {
+     querySnapshot.forEach( doc => {
+       console.log((doc.data().name),(doc.data().price))
+     })
+   })
+
+   db.collection('Resto do dia')
+   .doc('Acompanhamentos')
+   .collection('Acompanhamentos')
+   .get()
+   .then((querySnapshot) => {
+    querySnapshot.forEach( doc => {
+      console.log((doc.data().name),(doc.data().price))
+    })
   })
+  
+
+
   return (
     <div className="App">
       <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-          Learn React
-        </a>
+        <p>Burger Queen</p>
       </header>
     </div>
   );
