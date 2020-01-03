b1abc7ceb205dc5578291c8bfccd015cd184ee61
Jack Mattar <jackeline_mattar@hotmail.com>
2020-01-03 05:01:47 -0300

    novo componente para listar as opções e extras selecionados

---
+++ burger-queen/src/components/optionsUl.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {StyleSheet, css} from 'aphrodite';
+
+const OptionsUl = (props) => {
+    return(
+        props.options
+        ? (   
+            <ul className={css(styles.extras)}>
+                <li>{props.options}</li>
+                {props.additionals.length !== 0 ?
+                    (   <>
+                        <li>{props.additionals[0] + ' extra'}</li>
+                        {
+                            props.additionals[1]
+                            ? <li >
+                                {props.additionals[1]  + 'extra'}
+                              </li>
+                            : null
+                        }
+                        </>
+                    )
+                    : null
+                }
+            </ul> 
+        )
+        : null
+    );
+};
+
+const styles = StyleSheet.create({
+    extras: {
+        marginTop: -15,
+        listStyleType: 'none'
+    },
+})
+
+export default OptionsUl;
