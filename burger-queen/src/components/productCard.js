46b3d8d7b179cd9af60a5086b9578a3e74d53823
Jack Mattar <jackeline_mattar@hotmail.com>
2020-01-03 04:58:04 -0300

    modificando as uls para componente optionsUl

--- burger-queen/src/components/productCard.js
+++ burger-queen/src/components/productCard.js
@@ -1,13 +1,17 @@
 import React, {useState} from 'react';
 import { StyleSheet, css } from 'aphrodite';
+import './options.css';
 
 const ProductCard = (props) => {
   const [show, setShow] = useState(false)
   const [display, setDisplay] = useState('none')
 
   const styles = StyleSheet.create({
+    flex: {
+      display: 'flex'
+    },
+
     productCard:{
-      display: 'flex',
       flexDirection: 'column',
       margin: '1.5vh',
     },
@@ -20,22 +24,38 @@ const ProductCard = (props) => {
 
     modal: {
       display: display,
-      height: '20vw',
-      width: '40vw',
-      backgroundColor: '#000',
+      padding: '1vw',
+      borderRadius: 20,
+      height: '30vh',
+      width: '35vw',
+      backgroundColor: '#0d0d0d',
       color: '#fff'
     },
 
     form: {
-      display: 'flex',
       justifyContent: 'space-around',
       flexDirection: 'row'
     },
+    
+    options:{
+      paddingLeft: '1.5vw',
+      width: '50%',
+    },
 
-    mainSection: {
-      display: 'flex',
-      flexDirection: 'row'
+    inputP: {
+      alignItems: 'center',
+      fontSize: '2.2vh'
+    },
+
+    sendButton: {
+      borderRadius: 15,
+      height: '6vw',
+      width: '15vw',
+      backgroundColor: '#F8D956',
+      border: 'none',
+      fontSize: 14,
     }
+
   })
 
   const showOptionsDiv = () => {
@@ -100,49 +120,49 @@ const ProductCard = (props) => {
   return (
     props.options?
     (
-      <section className={css(styles.mainSection)}>    
-        <div className={css(styles.productCard)} onClick={showOptionsDiv}>
-          <img
-            className={css(styles.imgProduct)} 
-            alt="product" 
-            src={props.img}
-          />
+      <section className={css(styles.mainSection, styles.flex)}>    
+        <div className={css(styles.productCard, styles.flex)} onClick={showOptionsDiv}>
+          <img className={css(styles.imgProduct)} alt="product" src={props.img}/>
           <span>
             {props.title}
             <br/> R$ {props.price}
           </span>
         </div>
         <div className={css(styles.modal)}>
-          <form className={css(styles.form)} onSubmit={(event)=> handleSubmit(event,props.item)}>
-            <div id='options'>
+          <form className={css(styles.form, styles.flex)} onSubmit={(event)=> handleSubmit(event,props.item)}>
+            <div id='options' className={css(styles.options)}>
               <h3>Opções de Hambúrguer</h3>
               {props.options.map((option) => {
                 return (
-                  <p key={option}>
-                    <input type='radio' value={option} name='options' key={option} defaultChecked/>
-                    <label>{option}</label>
+                  <p key={option} className={css(styles.flex ,styles.inputP)}>
+                    <input className='input' type='radio' value={option} name='options'
+                      key={option} id={option + props.id} defaultChecked/>
+                    <label htmlFor={option + props.id} >{option}</label>
                   </p>
                 )
               })}
             </div>
-            <div>
+            <div className={css(styles.options)}>
               <h3>Extras por R$1?</h3>
               {props.additionals.map(add => {
                 return (
-                  <p key={add}>
-                    <input type='checkbox' id='extras' value={add} name='options' key={add}/>
-                    <label>{add}</label>
+                  <p key={add} className={css(styles.inputP, styles.flex)}>
+                    <input className='input' type='checkbox' id={add + props.id}
+                      value={add} name='extras' key={add}/>
+                    <label htmlFor={add + props.id}>{add}</label>
                   </p>
                 )
               })}
+              <button className={css(styles.sendButton)}>
+                Adicionar ao pedido
+              </button>
             </div>
-            <button>Adicionar ao pedido</button>
           </form>
         </div>
       </section>
     )
     :
-    <div onClick={props.handleClick} id={props.id} className={css(styles.productCard)}>
+    <div onClick={props.handleClick} id={props.id} className={css(styles.productCard, styles.flex)}>
       <img
         className={css(styles.imgProduct)} 
         alt="product" 
@@ -156,4 +176,4 @@ const ProductCard = (props) => {
   );
 };
 
-export default ProductCard
+export default ProductCard;
--- burger-queen/src/components/productParagraph.js
+++ burger-queen/src/components/productParagraph.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
+import OptionsUl from'../components/optionsUl';
 
 const ProductParagraph =(props) =>{
     props.item.id = props.id;
@@ -30,37 +31,8 @@ const ProductParagraph =(props) =>{
     };
 
     return(
-        props.options
-        ? (
-            <>
-            <p className={css(styles.paragraph)}>
-                <span className={css(styles.title)}>
-                    {props.title}
-                </span>
-                <span className={css(styles.amount)}>
-                    <button className={css(styles.button)} onClick={()=>{deletItem(props.item, props.id)}}>
-                        -
-                    </button>
-                    <span className={css(styles.count)}>
-                        {props.count}
-                    </span>
-                    <button className={css(styles.button)} onClick={()=>{addItem(props.item)}}>
-                        +
-                    </button>
-                </span>
-                <span className={css(styles.price)}>
-                    {props.currency}
-                    {props.item.data.totalPrice*props.item.data.count}
-                </span>
-            </p>
-            <ul className={css(styles.extras)}>
-                <li>{props.options}</li>
-                <li>{props.additionals[0] ? props.additionals[0] + ' extra':null}</li>
-                <li>{props.additionals[1] ? props.additionals[1] + ' extra':null}</li>
-            </ul>
-            </>
-        )
-        : <p className={css(styles.paragraph)}>
+        <>
+         <p className={css(styles.paragraph)}>
             <span className={css(styles.title)}>
                 {props.title}
             </span>
@@ -80,6 +52,11 @@ const ProductParagraph =(props) =>{
                 {totalPrice}
             </span>        
         </p>
+        <OptionsUl
+            options={props.options}
+            additionals={props.additionals}
+        />    
+        </>
     );
 };
 
@@ -101,11 +78,6 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end',
         width: '10%',
         color: '#9c9898'
-    },
-
-    extras: {
-        marginTop: -10,
-        listStyleType: 'none'
     },
 
     amount: {
